beffd3387432f545ac5ef7fd9327070a23019a4a
undefinedyobra <brianmelly008@gmail.com>
2024-04-02 10:51:23 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+//code your solution here
+// Defines saturdayFun function declaration
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  
+  // Defines mondayWork function expression
+  const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Defines wrapAdjective function
+  function wrapAdjective(visualFlair = "*") {
+    return function (adjective) {
+      return `You are ${visualFlair}${adjective}${visualFlair}!`;
+    };
+  }
